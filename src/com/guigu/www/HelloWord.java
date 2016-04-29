2d794974618d860c822b8da040c5f412cc77b7d9
wangyuanBOSS <wangyuanboss@126.com>
2016-04-29 18:46:08 +0800

    做了次for循环

--- src/com/guigu/www/HelloWord.java
+++ src/com/guigu/www/HelloWord.java
@@ -2,6 +2,9 @@ package com.guigu.www;
 
 public class HelloWord {
 public static void main(String[] args) {
-	System.out.println("sys");
+	for(int i=0;i<10;i++){
+		System.out.println("sys");
+	}
+	System.out.println(new Object());
 }
 }
